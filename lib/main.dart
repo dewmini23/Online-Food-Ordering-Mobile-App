86bef6657d9274ef5a7dc3d87f81ba078023fb9c
unknown <dewmini.info@gmail.com>
2024-09-26 01:16:44 -0700

    Implemented google button and register screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:online_food_ordering_app/bottom_navigation_bar.dart';
+// import 'package:online_food_ordering_app/bottom_navigation_bar.dart';
 import 'package:online_food_ordering_app/consts/theme_data.dart';
 import 'package:online_food_ordering_app/providers/theme_provider.dart';
+import 'package:online_food_ordering_app/screens/authentication/login.dart';
+import 'package:online_food_ordering_app/screens/authentication/register.dart';
 import 'package:online_food_ordering_app/screens/inner_screen/product_details.dart';
 import 'package:online_food_ordering_app/screens/inner_screen/viewed_recently.dart';
 import 'package:online_food_ordering_app/screens/inner_screen/wishlist.dart';
@@ -29,7 +31,8 @@ class MyApp extends StatelessWidget {
           title: 'Simply Yummy Restaurant',
           theme: Styles.themeData(
               isDarkTheme: themeProvider.getIsDarkTheme, context: context),
-          home: const RootScreen(),
+          // home: const RootScreen(),
+          home: const LoginScreen(),
           routes: {
             ProductDetailsScreen.routName: (context) =>
                 const ProductDetailsScreen(),
@@ -37,6 +40,7 @@ class MyApp extends StatelessWidget {
             // ignore: equal_keys_in_map
             ViewedRecentlyScreen.routName: (context) =>
                 const ViewedRecentlyScreen(),
+            RegisterScreen.routName: (context) => const RegisterScreen(),
           },
         );
       }),
